480697d22aca603992d3a9a0ffe1ea2cc5d2b5d8
novanish <anishchhetri196@gmail.com>
2024-08-03 20:59:36 +0545

    feat: Implement user registration, login, and logout in authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,49 @@
+const { StatusCodes } = require("http-status-codes");
+const ms = require("ms");
+
+const User = require("../models/User");
+const BadRequestError = require("../errors/BadRequestError");
+const UnAuthenticatedError = require("../errors/UnAuthenticatedError");
+const { TOKEN_COOKIE_OPTIONS } = require("../utils/constants");
+
 const register = async (req, res) => {
-  res.send("Register Route");
+  const { role, ...user } = req.body;
+
+  const emailAlreadyExists = await User.doesUserExistsWithEmail(user.email);
+  if (emailAlreadyExists) {
+    throw new BadRequestError("Email already exists");
+  }
+
+  const createdUser = await User.create(user);
+  const token = createdUser.createJWT();
+
+  res.cookie("token", token, TOKEN_COOKIE_OPTIONS);
+  res.status(StatusCodes.CREATED).json({ user: createdUser });
 };
 
 const login = async (req, res) => {
-  res.send("Login Route");
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    throw new UnAuthenticatedError("Invalid email or password");
+  }
+
+  const isPasswordValid = await user.verifyPassword(password);
+  if (!isPasswordValid) {
+    throw new UnAuthenticatedError("Invalid email or password");
+  }
+
+  const token = user.createJWT();
+
+  res.cookie("token", token, TOKEN_COOKIE_OPTIONS);
+  res.status(StatusCodes.OK).json({ user });
 };
 
-const logout = async (req, res) => {
-  res.send("Logout Route");
+const logout = (_, res) => {
+  const expiredDate = new Date(Date.now() - ms("1m"));
+  res.cookie("token", "", { expires: expiredDate });
+  res.status(StatusCodes.OK).send("Logout successful");
 };
 
 module.exports = {
