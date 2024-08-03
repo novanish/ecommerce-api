3d4edf2e9245f6a663fd49e0b26b56bc5f0ff3b2
novanish <anishchhetri196@gmail.com>
2024-08-03 21:00:32 +0545

    feat: Enhance app with cookie parsing and user routes

--- app.js
+++ app.js
@@ -3,20 +3,24 @@ require("express-async-errors");
 
 const express = require("express");
 const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
 
 const connectDB = require("./db/connect");
 const notFoundMiddleware = require("./middlewares/notFound");
 const errorHandlerMiddleware = require("./middlewares/errorHandler");
 
 const authRouter = require("./routes/authRoutes");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(morgan("tiny"));
 app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
