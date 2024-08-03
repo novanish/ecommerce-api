2f79a5ebea126929458e1b0bed35ef37f89addff
novanish <anishchhetri196@gmail.com>
2024-08-03 08:01:25 +0545

    feat: Enhance error handler middleware with custom error handling

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,6 +1,13 @@
 const { StatusCodes } = require("http-status-codes");
+const CustomAPIError = require("../errors/CustomAPIError");
 
 const errorHandler = (err, req, res, next) => {
+  console.error(err);
+
+  if (err instanceof CustomAPIError) {
+    return res.status(err.statusCode).json({ message: err.message });
+  }
+
   res
     .status(StatusCodes.INTERNAL_SERVER_ERROR)
     .json({ message: "Internal server error" });
