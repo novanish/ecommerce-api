4e795e4dab1a266ce9f3c0cef8f1b950abec414a
novanish <anishchhetri196@gmail.com>
2024-08-05 21:33:45 +0545

    feat: Implement product controller with CRUD operations

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,17 +1,46 @@
-const getAllProducts = async (req, res) => {
-  res.send("All products");
+const { StatusCodes } = require("http-status-codes");
+const Product = require("../models/Product");
+const NotFoundError = require("../errors/NotFoundError");
+const UnauthorizedError = require("../errors/UnauthorizedError");
+
+const getAllProducts = async (_, res) => {
+  const products = await Product.find({}).sort("updateAt createdAt");
+  res.status(StatusCodes.OK).json({ products });
 };
 
 const getProductById = async (req, res) => {
-  res.send("Single product");
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  if (!product) {
+    throw new NotFoundError(`Product with id ${id} not found`);
+  }
+
+  return res.status(StatusCodes.OK).json({ product });
 };
 
 const createProduct = async (req, res) => {
-  res.send("Create product");
+  const productData = req.body;
+  productData.user = req.user.userId;
+
+  const product = await Product.create(productData);
+  res.status(StatusCodes.CREATED).json({ product });
 };
 
 const updateProduct = async (req, res) => {
-  res.send("Update product");
+  const { id } = req.params;
+
+  const product = await Product.findById(id);
+  if (!product) {
+    throw new NotFoundError(`Product with id ${id} not found`);
+  }
+
+  const currentlyLoggedInUserId = req.user.userId;
+  checkOwnership(product, currentlyLoggedInUserId);
+
+  Object.assign(product, req.body);
+  await product.save();
+
+  res.status(StatusCodes.OK).json({ product });
 };
 
 const uploadProductImage = async (req, res) => {
@@ -19,14 +48,33 @@ const uploadProductImage = async (req, res) => {
 };
 
 const deleteProduct = async (req, res) => {
-  res.send("Delete product");
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  if (!product) {
+    throw new NotFoundError(`Product with id ${id} not found`);
+  }
+
+  const currentlyLoggedInUserId = req.user.userId;
+  checkOwnership(product, currentlyLoggedInUserId);
+
+  await product.deleteOne();
+  res.status(StatusCodes.NO_CONTENT).send();
 };
 
+function checkOwnership(product, userId) {
+  const productOwnerId = product.user.toString();
+  const isOwner = userId === productOwnerId;
+
+  if (!isOwner) {
+    throw new UnauthorizedError("You are not allowed to delete this product");
+  }
+}
+
 module.exports = {
-  getAllProducts,
-  getProductById,
   createProduct,
   updateProduct,
-  uploadProductImage,
   deleteProduct,
+  getAllProducts,
+  getProductById,
+  uploadProductImage,
 };
