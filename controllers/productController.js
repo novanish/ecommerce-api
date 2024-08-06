25eb0e57a12be9f190e3b83d5daddf438f118bc9
novanish <anishchhetri196@gmail.com>
2024-08-06 20:50:47 +0545

    feat: Implement product image upload functionality

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,12 @@
+const path = require("node:path");
+const crypto = require("node:crypto");
+
 const { StatusCodes } = require("http-status-codes");
+
 const Product = require("../models/Product");
 const NotFoundError = require("../errors/NotFoundError");
 const UnauthorizedError = require("../errors/UnauthorizedError");
+const BadRequestError = require("../errors/BadRequestError");
 
 const getAllProducts = async (_, res) => {
   const products = await Product.find({}).sort("updateAt createdAt");
@@ -44,7 +49,22 @@ const updateProduct = async (req, res) => {
 };
 
 const uploadProductImage = async (req, res) => {
-  res.send("Upload product image");
+  const image = req.files?.image;
+  if (!image) {
+    throw new BadRequestError("Please upload a file");
+  }
+
+  if (!image.mimetype.startsWith("image")) {
+    throw new BadRequestError("Please upload an image file");
+  }
+
+  const imageExtension = path.extname(image.name);
+  const imageFileName = crypto.randomBytes(20).toString("hex");
+  const imageName = `${imageFileName}${imageExtension}`;
+  const uploadPath = path.join(__dirname, "..", "public", "uploads", imageName);
+
+  await image.mv(uploadPath);
+  res.status(StatusCodes.OK).json({ imageUrl: `/uploads/${imageName}` });
 };
 
 const deleteProduct = async (req, res) => {
